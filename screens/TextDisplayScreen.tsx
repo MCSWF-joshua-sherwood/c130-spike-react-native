852ab5df0fb2312f06b0744da12f5b8869630a31
Sherwood Sgt Joshua <joshua.sherwood.mil@usmc.mil>
2025-08-05 15:20:19 -0500

    got prototype running and sqlite functioning

---
+++ screens/TextDisplayScreen.tsx
@@ -0,0 +1,49 @@
+import { Text, View } from '@/components/Themed';
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { StyleSheet } from 'react-native';
+import { useEffect, useState } from 'react';
+import * as SQLite from 'expo-sqlite';
+
+interface Row {
+  id: number;
+  value: string;
+}
+
+export default function TextDisplayScreen() {
+  const [values, setValues] = useState<string[]>([]);
+
+  async function loadValues(): Promise<void> {
+  const db = await SQLite.openDatabaseAsync('test_db');
+    const allRows = await db.getAllAsync<Row>('SELECT * FROM test');
+    setValues(allRows.map(row => row.value));
+  }
+
+  useEffect(() => {
+    loadValues();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {values.map((value, index) => (
+        <Text key={index}>{value}</Text>
+      ))}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
---
+++ screens/TextInputScreen.tsx
@@ -0,0 +1,62 @@
+import { View } from '@/components/Themed';
+import { Button, GestureResponderEvent, StyleSheet, TextInput } from 'react-native';
+import { useState } from 'react';
+import * as SQLite from 'expo-sqlite';
+
+export default function TextInputScreen() {
+  const [val, setVal] = useState('');
+  const [disabled, setDisabled] = useState(false);
+
+  async function saveText(event: GestureResponderEvent) {
+    setDisabled(true);
+    const db = await SQLite.openDatabaseAsync('test_db');
+    console.info('db connected');
+    try {
+      await db.runAsync('INSERT INTO test (value) VALUES (?)', val);
+    } catch (err) {
+      console.error(err);
+    }
+    console.info('db saved');
+    setDisabled(false);
+  }
+
+  async function resetDB(event: GestureResponderEvent) {
+    setDisabled(true);
+    const db = await SQLite.openDatabaseAsync('test_db');
+    try {
+      await db.execAsync(`
+        drop table if exists test;
+        create table test (id integer primary key not null, value text not null);
+        insert into test (value) values ('Auto-generated db text');
+    `);
+    } catch (err) {
+      console.error(err);
+    }
+    setDisabled(false);
+  }
+
+  return (
+    <View style={styles.container}>
+      <TextInput placeholder='Placeholder Text no?' onChangeText={setVal} value={val} />
+      <Button title='save' disabled={disabled} onPress={saveText} />
+      <Button title='reset db' disabled={disabled} onPress={resetDB} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%'
+  }
+});
